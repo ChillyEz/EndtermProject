178a3c723ed1d19adc4cdad575f7a9985e810048
chillyez <bayseitov.2016@gmail.com>
2026-09-22 21:05:17 +0500

    heroes page refactoring first test version

---
+++ heroes.js
@@ -0,0 +1,374 @@
+// asg
+export const heroes = [
+    {
+        name: "Meepo",
+        attribute: "Agility",
+        complexity: 3,
+        attackType: ["Melee"],
+        roles: ["Carry", "Durable", "Nuker", "Disabler", "Escape", "Pusher", "Initiator", "Jungler"],
+        image: "heroes/icons/meepo.jpg"
+    },
+
+    {
+        name: "Axe",
+        attribute: "Strength",
+        complexity: 1,
+        attackType: ["Melee"],
+        roles: ["Carry", "Durable", "Initiator", "Disabler", "Jungler"],
+        image: "heroes/icons/axe.jpg"
+    },
+
+    {
+        name: "Crystal Maiden",
+        attribute: "Intelligence",
+        complexity: 1,
+        attackType: ["Ranged"],
+        roles: ["Support", "Disabler", "Nuker", "Jungler"],
+        image: "heroes/icons/crystal_maiden.jpg"
+
+    },
+
+    {
+        name: "Invoker",
+        attribute: "Intelligence",
+        complexity: 3,
+        attackType: ["Ranged"],
+        roles: ["Carry", "Support", "Nuker", "Disabler", "Escape", "Pusher"],
+        image: "heroes/icons/invoker.jpg"
+    },
+
+    {
+        name: "Juggernaut",
+        attribute: "Agility",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Carry", "Pusher", "Escape", "Nuker", "Initiator"],
+        image: "heroes/icons/juggernaut.jpg"
+    },
+
+    {
+        name: "Pudge",
+        attribute: "Strength",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Disabler", "Initiator", "Durable", "Nuker"],
+        image: "heroes/icons/pudge.jpg"
+    },
+
+    {
+        name: "Phantom Assassin",
+        attribute: "Agility",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Carry", "Escape", "Nuker"],
+        image: "heroes/icons/phantom_assassin.jpg"
+    },
+
+    {
+        name: "Shadow Fiend",
+        attribute: "Agility",
+        complexity: 3,
+        attackType: ["Ranged"],
+        roles: ["Carry", "Nuker", "Pusher", "Disabler", "Jungler"],
+        image: "heroes/icons/shadow_fiend.jpg"
+    },
+
+    {
+        name: "Tidehunter",
+        attribute: "Strength",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Initiator", "Durable", "Disabler", "Nuker"],
+        image: "heroes/icons/tidehunter.jpg"
+    },
+
+    {
+        name: "Windranger",
+        attribute: "Universal",
+        complexity: 2,
+        attackType: ["Ranged"],
+        roles: ["Carry", "Support", "Disabler", "Nuker", "Escape"],
+        image: "heroes/icons/windranger.jpg"
+    },
+
+    {
+        name: "Lina",
+        attribute: "Intelligence",
+        complexity: 2,
+        attackType: ["Ranged"],
+        roles: ["Support", "Carry", "Nuker", "Disabler"],
+        image: "heroes/icons/lina.jpg"
+    },
+
+    {
+        name: "Anti-Mage",
+        attribute: "Agility",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Carry", "Escape", "Nuker"],
+        image: "heroes/icons/anti_mage.jpg"
+    },
+
+    {
+        name: "Drow Ranger",
+        attribute: "Agility",
+        complexity: 2,
+        attackType: ["Ranged"],
+        roles: ["Carry", "Disabler", "Pusher"],
+        image: "heroes/icons/drow_ranger.jpg"
+    },
+
+    {
+        name: "Bristleback",
+        attribute: "Strength",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Durable", "Initiator", "Disabler"],
+        image: "heroes/icons/bristleback.jpg"
+    },
+
+    {
+        name: "Faceless Void",
+        attribute: "Agility",
+        complexity: 3,
+        attackType: ["Melee"],
+        roles: ["Carry", "Initiator", "Disabler", "Escape"],
+        image: "heroes/icons/faceless_void.jpg"
+    },
+
+    {
+        name: "Lifestealer",
+        attribute: "Strength",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Carry", "Durable", "Disabler", "Nuker"],
+        image: "heroes/icons/lifestealer.jpg"
+    },
+
+    {
+        name: "Queen of Pain",
+        attribute: "Intelligence",
+        complexity: 3,
+        attackType: ["Ranged"],
+        roles: ["Carry", "Nuker", "Escape"],
+        image: "heroes/icons/queen_of_pain.jpg"
+    },
+
+    {
+        name: "Riki",
+        attribute: "Agility",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Carry", "Escape", "Disabler"],
+        image: "heroes/icons/riki.jpg"
+    },
+
+    {
+        name: "Slark",
+        attribute: "Agility",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Carry", "Escape", "Disabler"],
+        image: "heroes/icons/slark.jpg"
+    },
+
+    {
+        name: "Sniper",
+        attribute: "Agility",
+        complexity: 2,
+        attackType: ["Ranged"],
+        roles: ["Carry", "Nuker"],
+        image: "heroes/icons/sniper.jpg"
+    },
+
+    {
+        name: "Storm Spirit",
+        attribute: "Intelligence",
+        complexity: 3,
+        attackType: ["Ranged"],
+        roles: ["Carry", "Escape", "Nuker", "Disabler"],
+        image: "heroes/icons/storm_spirit.jpg"
+    },
+
+    {
+        name: "Tinker",
+        attribute: "Intelligence",
+        complexity: 3,
+        attackType: ["Ranged"],
+        roles: ["Carry", "Nuker", "Pusher"],
+        image: "heroes/icons/tinker.jpg"
+    },
+
+    {
+        name: "Viper",
+        attribute: "Agility",
+        complexity: 2,
+        attackType: ["Ranged"],
+        roles: ["Carry", "Durable", "Nuker"],
+        image: "heroes/icons/viper.jpg"
+    },
+
+    {
+        name: "Witch Doctor",
+        attribute: "Intelligence",
+        complexity: 2,
+        attackType: ["Ranged"],
+        roles: ["Support", "Nuker", "Disabler"],
+        image: "heroes/icons/witch_doctor.jpg"
+    },
+
+    {
+        name: "Zeus",
+        attribute: "Intelligence",
+        complexity: 3,
+        attackType: ["Ranged"],
+        roles: ["Support", "Nuker", "Disabler"],
+        image: "heroes/icons/zeus.jpg"
+    },
+    
+    {
+        name: "Bounty Hunter",
+        attribute: "Agility",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Carry", "Escape", "Disabler"],
+        image: "heroes/icons/bounty_hunter.jpg"
+    },
+
+    {
+        name: "Earthshaker",
+        attribute: "Strength",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Support", "Initiator", "Disabler"],
+        image: "heroes/icons/earthshaker.jpg"
+    },
+
+    {
+        name: "Pugna",
+        attribute: "Intelligence",
+        complexity: 2,
+        attackType: ["Ranged"],
+        roles: ["Carry", "Nuker", "Pusher"],
+        image: "heroes/icons/pugna.jpg"
+    },
+
+    {
+        name: "Razor",
+        attribute: "Agility",
+        complexity: 2,
+        attackType: ["Ranged"],
+        roles: ["Carry", "Nuker", "Durable"],
+        image: "heroes/icons/razor.jpg"
+    },
+
+    {
+        name: "Troll Warlord",
+        attribute: "Agility",
+        complexity: 2,
+        attackType: ["Melee", "Ranged"],
+        roles: ["Carry", "Pusher", "Disabler"],
+        image: "heroes/icons/troll_warlord.jpg"
+    },
+
+    {
+        name:"Enigma",
+        attribute: "Universal",
+        complexity: 3,
+        attackType: ["Ranged"],
+        roles: ["Support", "Nuker", "Disabler"],
+        image: "heroes/icons/enigma.jpg"
+    },
+
+    {
+        name: "Bane",
+        attribute: "Universal",
+        complexity: 2,
+        attackType: ["Ranged"],
+        roles: ["Support", "Nuker", "Disabler"],
+        image: "heroes/icons/bane.jpg"
+    },
+
+    {
+        name:"Io",
+        attribute: "Universal",
+        complexity: 3,
+        attackType: ["Ranged"],
+        roles: ["Support", "Escape", "Disabler"],
+        image: "heroes/icons/io.jpg"
+    },
+
+    {
+        name:"Magnus",
+        attribute: "Universal",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Initiator", "Disabler", "Nuker"],
+        image: "heroes/icons/magnus.jpg"
+    },
+
+    {
+        name:"Marci",
+        attribute: "Universal",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Support", "Disabler", "Nuker"],
+        image: "heroes/icons/marci.jpg"
+    },
+
+    {
+        name:"Sand King",
+        attribute: "Universal",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Initiator", "Disabler", "Nuker"],
+        image: "heroes/icons/sand_king.jpg"
+    },
+
+    {
+        name:"Brewmaster",
+        attribute: "Universal",
+        complexity: 2,
+        attackType: ["Melee"],
+        roles: ["Initiator", "Disabler", "Nuker"],
+        image: "heroes/icons/brewmaster.jpg"
+    },
+
+    {
+        name:"Batrider",
+        attribute: "Universal",
+        complexity: 2,
+        attackType: ["Ranged"],
+        roles: ["Initiator", "Disabler", "Nuker"],
+        image: "heroes/icons/batrider.jpg"
+    },
+
+    {
+        name:"Beastmaster",
+        attribute: "Universal",
+        complexity: 3,
+        attackType: ["Melee"],
+        roles: ["Initiator", "Disabler", "Nuker"],
+        image: "heroes/icons/beastmaster.jpg"
+    },
+
+    {
+        name:"Techies",
+        attribute: "Universal",
+        complexity: 3,
+        attackType: ["Ranged"],
+        roles: ["Nuker", "Disabler", "Pusher"],
+        image: "heroes/icons/techies.jpg"
+    },
+
+    {
+        name:"Venomancer",
+        attribute: "Universal",
+        complexity: 2,
+        attackType: ["Ranged"],
+        roles: ["Nuker", "Disabler", "Pusher"],
+        image: "heroes/icons/venomancer.jpg"
+        
+    }
+
+];
--- script.js
+++ script.js
@@ -1,3 +1,97 @@
+import { heroes } from "./heroes.js";
+
+const heroSections = document.querySelector("#hero-sections");
+const searchInput = document.querySelector("#hero-search");
+const attributeFilter = document.querySelector("#attribute-filter");
+const complexityFilter = document.querySelector("#complexity-filter");
+
+const attributes = [
+    "Strength",
+    "Agility",
+    "Intelligence",
+    "Universal"
+];
+const attributeImages = {
+    Strength: "heroes/web-ui/strength.png",
+    Agility: "heroes/web-ui/agility.png",
+    Intelligence: "heroes/web-ui/intelligence.png",
+    Universal: "heroes/web-ui/universal.png"
+};
+
+function renderHeroes(heroList) {
+
+    heroSections.innerHTML = "";
+
+    attributes.forEach(function(attribute) {
+
+        const section = document.createElement("section");
+        section.classList.add("hero-section");
+
+        section.innerHTML = `
+            <h3 class="attribute-title">
+            <img src="${attributeImages[attribute]}" alt="${attribute}">
+            <span>${attribute}</span>
+            </h3>
+
+            <div class="hero-gallery"></div>
+        `;
+
+        const gallery = section.querySelector(".hero-gallery");
+
+        heroList.forEach(function(hero) {
+
+            if (hero.attribute === attribute) {
+
+                const card = document.createElement("article");
+                card.classList.add("hero-card");
+
+                card.innerHTML = `
+                <img src="${hero.image}" alt="${hero.name}">
+                <div class="hero-caption">${hero.name}</div>
+                    `;
+
+                gallery.appendChild(card);
+            }
+        });
+
+        heroSections.appendChild(section);
+    });
+}
+searchInput.addEventListener("input", filterHeroes);
+
+attributeFilter.addEventListener("change", filterHeroes);
+
+complexityFilter.addEventListener("change", filterHeroes);
+
+renderHeroes(heroes);
+
+function filterHeroes() {
+
+    const searchText = searchInput.value.toLowerCase();
+    const selectedAttribute = attributeFilter.value;
+    const selectedComplexity = complexityFilter.value;
+
+    const filteredHeroes = heroes.filter(function(hero) {
+
+        const matchesSearch =
+            hero.name.toLowerCase().includes(searchText);
+
+        const matchesAttribute =
+            selectedAttribute === "All" ||
+            hero.attribute === selectedAttribute;
+
+        const matchesComplexity =
+            selectedComplexity === "All" ||
+            hero.complexity === Number(selectedComplexity);
+
+        return matchesSearch &&
+               matchesAttribute &&
+               matchesComplexity;
+    });
+
+    renderHeroes(filteredHeroes);
+}
+
 if (window.location.pathname.endsWith("heroes.html")) {
 const form = document.querySelector("#hero-form");
 
